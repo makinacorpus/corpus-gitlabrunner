86a69658fb8b83d6d7d40a2ea261d61fea00fa4f
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2017-04-26 11:41:20 +0200

    Use new corpusops orga

--- misc/create_centos_lxc_template.sh
+++ misc/create_centos_lxc_template.sh
@@ -5,4 +5,4 @@ lxc-create -t centos -n centos -- -R 7
 sed -i -re "s/lxc.network.link =.*/lxc.network.link = copslxcbr/g" /var/lib/lxc/centos/config 
 sed -i -re "s/lxc.utsname =.*/lxc.utsname = centos/g" /var/lib/lxc/centos/config 
 bin/ansible-playbook-wrapper -c local -i localhost, \
-    /srv/corpusops/corpusops.bootstrap/roles/corpusops.lxc_create/role.yml -e "lxc_container_name=copscentos from_container=centos"
+    /srv/corpusops/corpusops.bootstrap/roles/corpusops.roles/lxc_create/role.yml -e "lxc_container_name=copscentos from_container=centos"
